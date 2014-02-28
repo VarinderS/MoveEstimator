35c45c1f09e797b575154d912cad3be5a97470b8
Varinder @ home <highpolyboy@hotmail.com>
2014-03-01 12:33:56 +1300

    Add support for locations
    - Crud for locations
    - Css alterations

---
+++ MoveEstimator/Controllers/LocationsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoveEstimator.Models;
+
+namespace MoveEstimator.Controllers
+{
+    public class LocationsController : Controller
+    {
+        //
+        // GET: /Locations/
+		protected readonly Db db = new Db();
+
+        public ActionResult Index()
+        {
+			var locations = db.Locations.ToList();
+
+			var locationViewModel = new LocationViewModel();
+			locationViewModel.LocationList = locations;
+
+			return View(locationViewModel);
+        }
+
+		public ActionResult Update(Location location)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(location).State = EntityState.Modified;
+				db.SaveChanges();
+			}
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Add(Location locationModel)
+		{
+			if (ModelState.IsValid)
+			{
+				if (db.Locations.Any( location => location.Name == locationModel.Name ))
+				{
+					TempData["AreadyExsists"] = "This location already exisits";
+				}
+				else
+				{
+					db.Locations.Add(locationModel);
+					db.SaveChanges();
+				}
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Delete(int Id = 0)
+		{
+			var location = db.Locations.Find(Id);
+
+			if (location == null) 
+			{
+				return HttpNotFound();
+			}
+
+			return View(location);
+		}
+
+		public ActionResult DeleteConfirmed(int Id = 0)
+		{
+			var location = db.Locations.Find(Id);
+
+			if (location == null)
+			{
+				return HttpNotFound();
+			}
+			else
+			{
+				db.Locations.Remove(location);
+				db.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
+		}
+
+    }
+}
---
+++ MoveEstimator/Models/LocationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoveEstimator.Models
+{
+	public class LocationViewModel
+	{
+		public LocationViewModel()
+		{
+			this.LocationModel = new Location();
+		}
+		public IEnumerable<Location> LocationList { get; set; }
+		public Location LocationModel { get; set; }
+	}
+}
